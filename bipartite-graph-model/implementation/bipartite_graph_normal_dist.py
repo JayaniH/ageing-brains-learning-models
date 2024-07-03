497044f2bb2e92552e11f53575b737e975f3c99b
JayaniH <43147809+JayaniH@users.noreply.github.com>
2024-07-03 16:36:13 +0100

    Update weight generation

--- bipartite-graph-model/implementation/bipartite_graph_normal_dist.py
+++ bipartite-graph-model/implementation/bipartite_graph_normal_dist.py
@@ -3,31 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from constants import BASE_PATH
 
-# Creating a bipartite graph
-input_n = np.zeros((30, 30))  # Initialize matrix with zeros
+# Check if the file exists
+file_path = os.path.join(BASE_PATH, 'data/original_data/Inputs_normal_dis.csv')
+if os.path.isfile(file_path):
+    # Read the matrix from the file
+    print("Reading data from file")
+    input_n = np.loadtxt(file_path, delimiter=',')
+else:
+    # Generate new data
+    print("Generating new data")
 
-# Get the dimensions of the graph
-n_node_n, n_edge_n = input_n.shape
+    # Creating a bipartite graph
+    input_n = np.zeros((30, 30))  # Initialize matrix with zeros
 
-# Loop for every node in the input nodes
-for a in range(n_node_n):
-    # Create 30 random numbers for each edge of that node using normal distribution
-    w_n = 0.3 * np.random.randn(n_edge_n) + 1
-    
-    # Normalize to sum to 1
-    w_norm_n = w_n / np.sum(w_n)
-    
-    # Put normalized values into matrix created before
-    input_n[a, :] = w_norm_n
+    # Get the dimensions of the graph
+    n_node_n, n_edge_n = input_n.shape
+    for a in range(n_node_n):
+        # Create 30 random numbers for each edge of that node using normal distribution
+        w_n = 0.3 * np.random.randn(n_edge_n) + 1
 
-# Print the resulting matrix (optional)
-print(input_n)
+        # Normalize to sum to 1
+        w_norm_n = w_n / np.sum(w_n)
+
+        # Put normalized values into matrix created before
+        input_n[a, :] = w_norm_n
 
 # Calculate the sum of the signal arriving at each output node (column-wise sum)
 output_n = np.sum(input_n, axis=0)
-
-# Print the result
-print(output_n)
 
 # Calculate variance
 variance_n = np.var(output_n)
@@ -39,39 +41,70 @@ minimum_n = np.min(output_n)
 maximum_n = np.max(output_n)
 
 # Print the results
+print("Output statistics (Normal distribution)")
 print("Variance:", variance_n)
 print("Minimum:", minimum_n)
 print("Maximum:", maximum_n)
 
-
+# Plot the data - subplots
 # Set up figure environment
-plt.figure(figsize=(12, 8))
-
-# Plot heatmap for output_n
-plt.subplot(3, 2, 4)
-h_n = plt.imshow(output_n.reshape(-1, 1), cmap='winter', aspect='auto')
-plt.colorbar(h_n)
-plt.title('Heatmap - output signal in nodes (Normal distribution)', fontsize=12)
-plt.ylabel('Nodes')
-plt.xlabel('Heatmap')
+plt.figure(figsize=(5, 8))
 
 # Plot histogram for input_n
-plt.subplot(3, 2, 2)
-iprandn = plt.hist(input_n.flatten(), bins=10, color=[0.2, 0.2, 0.9], alpha=0.7)
-plt.ylabel('Number of edges', fontsize=14)
-plt.xlabel('Weight signal', fontsize=14)
+plt.subplot(3, 1, 1)
+iprandn = plt.hist(input_n.flatten(), bins=10, color=[0.2, 0.2, 0.9], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of edges', fontsize=10)
+plt.xlabel('Weight signal', fontsize=10)
 plt.ylim([0, 300])
-plt.title('Normal distribution', fontsize=14)
+plt.title('Weight assignment distribution curve for edges\n(Normal distribution)', fontsize=12)
+
+# Plot heatmap for output_n
+plt.subplot(3, 1, 2)
+h_n = plt.imshow(output_n.reshape(-1, 1), cmap='winter', aspect='auto')
+plt.colorbar(h_n)
+plt.xticks([]) 
+plt.title('Heatmap - output signal in nodes\n(Normal distribution)', fontsize=12)
+plt.ylabel('Nodes')
+# plt.xlabel('Heatmap')
 
 # Plot histogram for output_n
-plt.subplot(3, 2, 6)
-bars = plt.hist(output_n, bins=10, color=[0.2, 0.9, 0.2], alpha=0.7)
-plt.ylabel('Number of nodes', fontsize=14)
-plt.xlabel('Signal', fontsize=14)
+plt.subplot(3, 1, 3)
+bars = plt.hist(output_n, bins=10, color=[0.2, 0.9, 0.2], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of nodes', fontsize=10)
+plt.xlabel('Signal', fontsize=10)
+plt.title('Resulting Output signal distribution\n(Normal distribution)', fontsize=12)
 plt.ylim([0, 10])
 
 plt.tight_layout()
+plt.savefig(os.path.join(BASE_PATH, 'figures/normal_distribution.png'))
 plt.show()
 
-# Uncomment to generate new edge weights to use in next scripts
-np.savetxt(os.path.join(BASE_PATH, 'data/inputs_normal_dis.csv'), input_n, delimiter=',')
+
+# Plot the data - separate plots
+# Plot histogram for input_n
+plt.hist(input_n.flatten(), bins=10, color=[0.2, 0.2, 0.9], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of edges', fontsize=10)
+plt.xlabel('Weight signal', fontsize=10)
+plt.title('Input Weights (Normal Distribution)', fontsize=12)
+plt.savefig(os.path.join(BASE_PATH, 'figures/normal_distribution_input.png'))
+plt.show()
+
+# Plot histogram for output_n
+plt.hist(output_n, bins=10, color=[0.2, 0.9, 0.2], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of nodes', fontsize=10)
+plt.xlabel('Signal', fontsize=10)
+plt.title('Output Signals (Normal Distribution)', fontsize=12)
+plt.savefig(os.path.join(BASE_PATH, 'figures/normal_distribution_output.png'))
+plt.show()
+
+# Plot heatmap for output_n
+plt.imshow(output_n.reshape(-1, 1), cmap='winter', aspect='auto')
+plt.colorbar()
+plt.xticks([])
+plt.title('Heatmap - Output Signal in Nodes (Normal Distribution)', fontsize=12)
+plt.ylabel('Nodes', fontsize=10)
+plt.savefig(os.path.join(BASE_PATH, 'figures/normal_distribution_heatmap.png'))
+plt.show()
+
+if not os.path.isfile(file_path):
+    np.savetxt(os.path.join(BASE_PATH, 'data/inputs_normal_dis.csv'), input_n, delimiter=',')
--- bipartite-graph-model/implementation/bipartitie_graph_uniform_dist.py
+++ bipartite-graph-model/implementation/bipartitie_graph_uniform_dist.py
@@ -1,30 +1,38 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from constants import BASE_PATH
 
 # Using uniform distribution
+# Check if the file exists
+file_path = os.path.join(BASE_PATH, 'data/original_data/Inputs_uniform_dis.csv')
+if os.path.isfile(file_path):
+    # Read the matrix from the file
+    print("Reading data from file")
+    input_u = np.loadtxt(file_path, delimiter=',')
+else:
+    # Generate new data
+    print("Generating new data")
 
-# Create a 30x30 matrix initialized with zeros
-input_u = np.zeros((30, 30))
+    # Create a 30x30 matrix initialized with zeros
+    input_u = np.zeros((30, 30))
 
-# Get the dimensions of the graph
-n_node_u, n_edge_u = input_u.shape
+    # Get the dimensions of the graph
+    n_node_u, n_edge_u = input_u.shape
 
-# Loop for every node in the input nodes
-for i in range(n_node_u):
-    # Create 30 random numbers for each edge of that node
-    w_u = np.random.rand(n_edge_u)
-    
-    # Normalize to sum to 1
-    w_norm_u = w_u / np.sum(w_u)
-    
-    # Put normalized values into matrix created before
-    input_u[i, :] = w_norm_u
+    # Loop for every node in the input nodes
+    for i in range(n_node_u):
+        # Create 30 random numbers for each edge of that node
+        w_u = np.random.rand(n_edge_u)
+        
+        # Normalize to sum to 1
+        w_norm_u = w_u / np.sum(w_u)
+        
+        # Put normalized values into matrix created before
+        input_u[i, :] = w_norm_u
 
 # Calculate the sum of the signal arriving at each output node (column-wise sum)
 output_u = np.sum(input_u, axis=0)
-
-# Print the result
-print(output_u)
 
 # Calculate variance
 variance_u = np.var(output_u)
@@ -36,41 +44,70 @@ minimum_u = np.min(output_u)
 maximum_u = np.max(output_u)
 
 # Print the results
+print("Output statistics (Uniform distribution)")
 print("Variance:", variance_u)
 print("Minimum:", minimum_u)
 print("Maximum:", maximum_u)
 
 
+# Plot the data - subplots
 # Set up figure environment
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(5, 8))
 
 # Plot histogram for input_u
-plt.subplot(3, 2, 1)
-iprandu = plt.hist(input_u.flatten(), bins=10, color=[0.2, 0.2, 0.9], alpha=0.7)
-plt.text(0.04, 250, 'Weight assignment distribution curve for edges', fontsize=16, fontweight='bold')
-plt.ylabel('Number of edges', fontsize=14)
-plt.xlabel('Weight signal', fontsize=14)
+plt.subplot(3, 1, 1)
+iprandu = plt.hist(input_u.flatten(), bins=10, color=[0.2, 0.2, 0.9], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of edges', fontsize=10)
+plt.xlabel('Weight signal', fontsize=10)
 plt.ylim([0, 200])
-plt.title('Uniform distribution', fontsize=14)
+plt.title('Weight assignment distribution curve for edges\n(Uniform distribution)', fontsize=12)
 
 # Plot histogram for output_u
-plt.subplot(3, 2, 5)
-bars = plt.hist(output_u, bins=10, color=[0.2, 0.9, 0.2], alpha=0.7)
-plt.text(1.1, 11, 'Resulting Output signal distribution', fontsize=16, fontweight='bold')
-plt.ylabel('Number of nodes', fontsize=14)
-plt.xlabel('Signal', fontsize=14)
+plt.subplot(3, 1, 3)
+bars = plt.hist(output_u, bins=10, color=[0.2, 0.9, 0.2], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of nodes', fontsize=10)
+plt.xlabel('Signal', fontsize=10)
+plt.title('Output signal distribution in nodes\n(Uniform distribution)', fontsize=12)
 plt.ylim([0, 10])
 
 # Plot heatmap for output_u
-plt.subplot(3, 2, 3)
+plt.subplot(3, 1, 2)
 h_u = plt.imshow(output_u.reshape(-1, 1), cmap='winter', aspect='auto')
 plt.colorbar(h_u)
-plt.title('Heatmap - output signal in nodes (Uniform distribution)', fontsize=12)
+plt.title('Heatmap - output signal in nodes\n(Uniform distribution)', fontsize=12)
 plt.ylabel('Nodes')
 plt.xlabel('Heatmap')
 
 plt.tight_layout()
+plt.savefig(os.path.join(BASE_PATH, 'figures/uniform_distribution.png'))
 plt.show()
 
-# Uncomment to generate new edge weights to use in next scripts
-# np.savetxt(os.path.join(BASE_PATH, 'data/inputs_uniform_dis.csv'), input_u, delimiter=',')
+# Plot the data - separate plots
+# Plot histogram for input_u    
+plt.hist(input_u.flatten(), bins=10, color=[0.2, 0.2, 0.9], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of edges', fontsize=10)
+plt.xlabel('Weight signal', fontsize=10)
+plt.title('Input Weights (Uniform Distribution)', fontsize=12)
+plt.savefig(os.path.join(BASE_PATH, 'figures/uniform_distribution_input.png'))
+plt.show()
+
+# Plot histogram for output_u
+plt.hist(output_u, bins=10, color=[0.2, 0.9, 0.2], alpha=0.7, edgecolor='black')
+plt.ylabel('Number of nodes', fontsize=10)
+plt.xlabel('Signal', fontsize=10)
+plt.title('Output Signals (Uniform Distribution)', fontsize=12)
+plt.savefig(os.path.join(BASE_PATH, 'figures/uniform_distribution_output.png'))
+plt.show()
+
+# Plot heatmap for output_u
+plt.imshow(output_u.reshape(-1, 1), cmap='winter', aspect='auto')
+plt.colorbar()
+plt.title('Heatmap - Output Signal in Nodes (Uniform Distribution)', fontsize=12)
+plt.ylabel('Nodes')
+plt.xlabel('Heatmap')
+plt.savefig(os.path.join(BASE_PATH, 'figures/uniform_distribution_heatmap.png'))
+plt.show()
+
+# Save the data to a file
+if not os.path.isfile(file_path):
+    np.savetxt(os.path.join(BASE_PATH, 'data/inputs_uniform_dis.csv'), input_u, delimiter=',')
