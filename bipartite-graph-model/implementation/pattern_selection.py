62cd98eb4c74f4bd33e8d63e1d43803c2849e459
JayaniH <43147809+JayaniH@users.noreply.github.com>
2024-11-26 16:51:19 +0000

    Update and restructure pattern selection code

--- bipartite-graph-model/implementation/pattern_selection.py
+++ bipartite-graph-model/implementation/pattern_selection.py
@@ -1,12 +1,14 @@
 import os
 import numpy as np
-from pattern_selection_utils import get_input_and_output_patterns, get_threshold, get_valid_inputs_and_outputs, get_output_statistics, plot_signal_statistics
+from pattern_selection_utils import get_input_and_output_patterns, get_threshold, get_valid_inputs_and_outputs, get_output_statistics, plot_signal_statistics, get_threshold_open
 from constants import BASE_PATH
 
 def main():
 
+    data_file_path = 'original_data\Inputs_normal_dis'
+
     # Read the input matrix from CSV file
-    initial_weights = np.loadtxt(os.path.join(BASE_PATH, 'data\original_data\Inputs_normal_dis.csv'), delimiter=',')
+    initial_weights = np.loadtxt(os.path.join(BASE_PATH, f'data\{data_file_path}.csv'), delimiter=',')
 
     input_patterns, input, output = get_input_and_output_patterns(initial_weights)
 
@@ -16,6 +18,7 @@ def main():
     # Define threshold values to attempt
     th_values = [0.2, 0.2405, 0.28, 0.275, 0.278]
     threshold = get_threshold(th_values, output)
+    # threshold = get_threshold_open(output, percentage=0.25)
 
     valid_input_patterns, valid_input, valid_output = get_valid_inputs_and_outputs(threshold, input, input_patterns, output)
 
